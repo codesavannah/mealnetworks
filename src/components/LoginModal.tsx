09989e89e4d122cc9fb09200b4a1f855312d9dda
NuclearTeddy22 <nuclearteddy98@gmail.com>
2025-10-11 23:45:18 +0530

    Update login modal registration text to include Receiver option

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -164,7 +164,7 @@ export default function LoginModal({ open, onClose }: LoginModalProps) {
                     '&:hover': { textDecoration: 'underline' }
                   }}
                 >
-                  Register as Donor
+                  Register as Donor / Receiver
                 </Link>
               </Typography>
             </Box>
